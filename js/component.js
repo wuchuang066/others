30aea076cdf70c3b825f67d4fb2ec7a96609f44d
wuchuang <745418984@qq.com>
2019-07-07 12:19:41 +0800

    2019-07-07 上午

--- js/component.js
+++ js/component.js
@@ -1,4 +1,12 @@
 Vue.component('todo-item',{
 	props:['todo1'],
 	template:'<li>{{todo1.text}}</li>'
+	// methods:{
+	// 	reversedMessage:function(){
+	// 		return this.message.split('').reverse().join('')
+	// 	}
+	// }
+})
+Vue.component('my-component',{
+	template:'<p class = "foo bar">Hi</p>'
 })
